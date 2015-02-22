22030c08a3136f388701b53fc8c2b7d8878cc56a
Guo Yu <i@guoyu.me>
2015-02-23 01:23:44 +0800

    Replace ionic slides directive with Swiper

--- src/javascript/controllers/home.js
+++ src/javascript/controllers/home.js
@@ -11,7 +11,6 @@
       '$state',
       'Store',
       '$ionicLoading',
-      '$ionicSlideBoxDelegate',
       '$timeout',
       '$rootScope',
       'imageLoader',
@@ -20,32 +19,33 @@
       home
     ]);
 
-  function home(scope, $state, Store, $ionicLoading, $ionicSlideBoxDelegate, $timeout, $rootScope, imageLoader, $cordovaDialogs, share) {
-    scope.go = go;
+  function home(scope, $state, Store, $ionicLoading, $timeout, $rootScope, imageLoader, $cordovaDialogs, share) {
     scope.share = share.popup;
-    scope.updateSlides = updateSlides;
-    scope.backgrounds = imageLoader.loadCache('home');
+
+    var slides;
+    var backgrounds = scope.backgrounds = [];
+    // scope.backgrounds = imageLoader.loadCache('home');
 
     // Show loading message
-    $ionicLoading.show(
+    $ionicLoading.show({
       template: '<i class="icon ion-refreshing"></i> 努力加载中...'
-    );
+    });
 
     // Read local cache from localStorage
     if (Store.cache.collections)
       return setup(Store.cache.collections);
 
-    fetchFresh();
+    fetch();
 
     // Fetch fresh data from API server
-    function fetchFresh(callback) {
+    function fetch() {
       Store.hot.get({}, success, fail);
 
       function success(data){
         if (!data.collections) {
-          $ionicLoading.show(
+          $ionicLoading.show({
             template: '<i class="icon ion-close-circled"></i> 网络连接失败...请稍后再试'
-          );
+          });
           return;
         }
 
@@ -53,71 +53,55 @@
         setup(data.collections, true);
         // Save all data to cache
         Store.save('collections', data.collections);
-
-        if (callback)
-          callback();
       }
 
-      function fail(err){
-        $ionicLoading.show(
+      function fail(err) {
+        $ionicLoading.show({
           template: '<i class="icon ion-close-circled"></i> 网络连接失败...请稍后再试'
-        );
-
-        if (callback)
-          callback();
+        });
       }
     }
 
-    // Init a slides with 3 slides,
-    // If collection's pictures is above 3.
-    function setup(collections, fresh) {
-      var lastIndex = localStorage.lastSlideIndexHome;
-      var inValid = !lastIndex || parseInt(lastIndex) <= 3;
-      // Lazy loading slides with a center point
-      scope.collections = fresh ? 
-        angular.copy(collections).splice(0, 3) :
-        angular.copy(collections).splice(0, inValid ? 3 : parseInt(lastIndex) + 1);
+    function setup(data) {
+      scope.collections = data;
 
-      // Lazy loading the first slide's backgroud-image
-      imageLoader.load(0, scope, 'home');
-      
-      $ionicSlideBoxDelegate.update();
+      // Show befor init slides
+      if (!scope.slidesReady)
+        scope.slidesReady = true;
+
+      // Hide loading
+      $ionicLoading.hide();
+
+      // Lazy loading the first slide's backgroud image
+      loadImage(0);
+
+      // Init the slides on next tick
+      $timeout(function(){
+        slides = new Swiper('.swiper-container-collection');
+
+        // if children exist, render a sub vertical slides.
+        new Swiper('.swiper-container-children', {
+          direction: 'vertical'
+        });
+
+        // Load next page
+        loadImage(1);
+        loadImage(2);
+      }, 10);
     }
 
     // Update slides async
-    function updateSlides(index) {
-      if (isNaN(index)) index = 0;
+    function loadImage(index) {
+      if (backgrounds[index])
+        return;
 
-      console.log('Switching to slide: [%s]', index);
-
-      // Loading this slide's backgroud-image
-      imageLoader.load(index, scope, 'home');
-
-      // Load the next slides
-      if (!scope.collections[index + 1] && Store.cache.collections[index + 1])
-        scope.collections.push(Store.cache.collections[index + 1]);
-
-      // Update the latest index of home slides
-      localStorage.lastSlideIndexHome = index;
-
-      console.log('Set lastSlideIndexHome to %s', index);
-
-      $ionicSlideBoxDelegate.update();
-    }
-
-    // Go to the target state
-    function go(id, imagesLength) {
-      var isValidCollection = imagesLength > 1;
-
-      if (isValidCollection)
-        return $state.go('collection', {id: id});
-
-      $ionicLoading.show(
-        template: '<i class="icon ion-information-circled"></i> 这个相册只有一张图'
+      imageLoader.load(
+        scope.collections[index].images[0].uri + '.jpg',
+        function(localImage) {
+          scope.backgrounds[index] = localImage;
+          scope.$apply();
+        }
       );
-
-      $timeout($ionicLoading.hide, 400);
     }
   }
-
 })(this);
